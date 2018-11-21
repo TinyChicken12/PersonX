c06decd028780ab37f0c6e26936a4e2aac05fbdf
TinyChicken12 <tnkien12vcu@gmail.com>
2018-11-22 04:50:15 +0700

    add files

---
+++ src/Staff.java
@@ -0,0 +1,20 @@
+public class Staff extends Person {
+    private String school;
+    private double pay;
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public double getPay() {
+        return pay;
+    }
+
+    public void setPay(double pay) {
+        this.pay = pay;
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,29 @@
+public class Student extends Person {
+    protected String program;
+    protected int year;
+    protected double fee;
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+}
